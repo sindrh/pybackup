0a5e539128be73d0fd6042b5d75cbb73228bb49a
Sindre Hansen <sindrehansen86@gmail.com>
2020-03-03 20:16:10 +0100

    Place full and incremental backups in separate folders

--- backup.py
+++ backup.py
@@ -55,7 +55,9 @@ class EncryptedBackup(object):
         date_today = str(datetime.date.today())
         inc_name = "Backup_{}".format(date_today)
         self._incrementals_dir = Path(config["backup"]["target_dir"]) / "Incremental"
+        self._full_backups_dir = Path(config["backup"]["target_dir"]) / "Full"
         self._incremental_dir = Path(config["backup"]["target_dir"]) / "Incremental" / inc_name
+        self._full_backup_dir = Path(config["backup"]["target_dir"]) / "Full" / inc_name
         self._mirror_dir = Path(config["backup"]["target_dir"]) / "Mirror"
         self._src_dirs = Path(config["backup"]["src_dirs"])
         self._log = BackupLog(config["backup"]["log_dir"])
@@ -63,11 +65,15 @@ class EncryptedBackup(object):
         self._rsync_extra = config["backup"]["rsync_extra"]
         self._gpg_public_key = config["general"]["gpg_public_key"]
         self._dropbox_token = config["general"]["dropbox_token"]
-        self._dropbox_target = Path(config["general"]["dropbox_target"]) / Path(inc_name)
+        self._dropbox_target = Path(config["general"]["dropbox_target"])
         self._full_backup_interval = int(config["general"]["full_backup_interval"])
         self._encrypted_file = Path(config["backup"]["target_dir"]) / "{}.gpg".format(inc_name)
         self._tar_file = Path(config["backup"]["target_dir"]) / "{}.tar".format(inc_name)
         self._files_splitted = []
+        self._must_do_full_backup = False
+        dirs = list(self._incrementals_dir.glob("*")) + list(self._full_backups_dir.glob("*"))
+        if (len(dirs) % self._full_backup_interval == 0):
+            self._must_do_full_backup = True
 
     def _delete_file(self, file):
         subprocess.run(["rm", "-f", file])
@@ -75,33 +81,37 @@ class EncryptedBackup(object):
     def _delete_directory(self, directory):
         subprocess.run(["rm", "-rf", directory])
 
-    def _delete_mirror_if_full_backup(self):
-        dirs = list(self._incrementals_dir.glob("*"))
-        if (len(dirs) % self._full_backup_interval == 0):
-            try:
-                mailer.send("Starting full backup", "Starting full backup.")
-            except:
-                pass
-            self._log.write("Number of incremental backups is a multiple of " +
-                " {}.".format(self._full_backup_interval))
-            self._log.write("Time for a full backup.")
-            self._log.write("Removing directory of mirror backup.")
-            self._delete_directory(self._mirror_dir)
+    def _is_full_backup(self):
+        return self._must_do_full_backup
 
-    def _create_dirs(self):
-        self._log.write("Creating directory structure.")
-        self._incremental_dir.mkdir(parents=True)
+    def _delete_mirror(self):
+        self._log.write("Removing directory of mirror backup.")
+        self._delete_directory(self._mirror_dir)
+
+    def _notify_full_backup(self):
+        self._log.write("Number of incremental backups is a multiple of " +
+            " {}.".format(self._full_backup_interval))
+        self._log.write("Time for a full backup.")
+        try:
+            mailer.send("Starting full backup", "Starting full backup.")
+        except:
+            pass
+
+    def _create_mirror_dir(self):
+        self._log.write("Creating mirror directory structure.")
         self._mirror_dir.mkdir(exist_ok=True)
 
-    def _create_incremental(self):
-        self._log.write("Creating incremental backup in '{}'.".format(self._incremental_dir))
+    def _create_backup(self, target):
+        self._log.write("Creating target directory structure.")
+        target.mkdir(parents=True)
+        self._log.write("Creating backup in '{}'.".format(target))
         rsync_cmd = "{} -ac {} --progress {} --compare-dest={} {}".format(self._rsync,
-                self._src_dirs, self._incremental_dir, self._mirror_dir.absolute(),
+                self._src_dirs, target, self._mirror_dir.absolute(),
                 self._rsync_extra)
         self._log.write("Running: {}".format(rsync_cmd))
         rsync_proc = subprocess.run(rsync_cmd.split())
-        self._log.write("Removing write access to '{}'".format(self._incremental_dir))
-        ch_proc = subprocess.run(["chmod", "-R", "ugo-w", self._incremental_dir])
+        self._log.write("Removing write access to '{}'".format(target))
+        ch_proc = subprocess.run(["chmod", "-R", "ugo-w", target])
 
     def _create_mirror(self):
         self._log.write("Creating mirror in '{}'.".format(self._mirror_dir))
@@ -110,16 +120,16 @@ class EncryptedBackup(object):
         self._log.write("Running: {}".format(rsync_cmd))
         rsync_proc = subprocess.run(rsync_cmd.split())
 
-    def _compress_incremental(self):
+    def _compress_backup(self, target):
         self._delete_file(self._tar_file)
-        self._log.write("Creating tar file of incremental backup.")
-        tar_cmd = "tar cf {} {}".format(self._tar_file, self._incremental_dir)
+        self._log.write("Creating tar file of backup.")
+        tar_cmd = "tar cf {} {}".format(self._tar_file, target)
         self._log.write("Running: {}".format(tar_cmd))
         tar_proc = subprocess.run(tar_cmd.split())
 
-    def _encrypt_incremental(self):
+    def _encrypt_backup(self):
         self._delete_file(self._encrypted_file)
-        self._log.write("Encrypting incremental backup.")
+        self._log.write("Encrypting backup.")
         enc_cmd = "gpg -o {} --encrypt -r {} {}".format(self._encrypted_file, self._gpg_public_key, self._tar_file)
         self._log.write("Running: {}".format(enc_cmd))
         enc_proc = subprocess.run(enc_cmd.split())
@@ -129,7 +139,10 @@ class EncryptedBackup(object):
         try:
             f = self._encrypted_file
             self._log.write("Source file: {}".format(f))
-            target_file = self._dropbox_target / f.name
+            backup_type_dir = "Incremental"
+            if self._is_full_backup():
+                backup_type_dir = "Full"
+            target_file = self._dropbox_target / backup_type_dir / f.name
             self._log.write("Target file: {}".format(target_file))
             dropbox_backup.upload_file(file_path=str(f), dest_path=str(target_file), token=self._dropbox_token)
         except Exception as ex:
@@ -139,14 +152,18 @@ class EncryptedBackup(object):
             self._log.write("File uploaded successfully to dropbox.")
 
     def run(self):
-        self._log.write("Starting incremental backup.")
+        self._log.write("Starting backup.")
         self._log.write("Source directories is '{}'.".format(self._src_dirs))
-        self._delete_mirror_if_full_backup()
-        self._create_dirs()
-        self._create_incremental()
+        backup_target = self._incremental_dir
+        if self._is_full_backup():
+            backup_target = self._full_backup_dir
+            self._notify_full_backup()
+            self._delete_mirror()
+        self._create_mirror_dir()
+        self._create_backup(backup_target)
         self._create_mirror()
-        self._compress_incremental()
-        self._encrypt_incremental()
+        self._compress_backup(backup_target)
+        self._encrypt_backup()
         self._upload_to_dropbox()
         self._delete_file(self._encrypted_file)
         self._delete_file(self._tar_file)
